2640758458c4dd8d7dff845a8c0109b4885297de
Alexey <lexshadrin@mail.ru>
2022-06-25 15:34:12 +1100

    Начало ДЗ_6

---
+++ src/main/java/pro/sky/course_2_lesson_6/Course2Lesson6Application.java
@@ -0,0 +1,13 @@
+package pro.sky.cours_2_lesson_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours2Lesson6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cours2Lesson6Application.class, args);
+    }
+
+}
